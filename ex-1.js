217f015feee20f2b018bfc0aa337cb58dec81019
T <itthi91@gmail.com>
2023-12-02 20:37:28 +0700

    update: ex1

--- ex-1.js
+++ ex-1.js
@@ -1,7 +1,7 @@
 let colors = ["red", "green", "blue", "khaki", "orange"];
 
 // Start coding here
-let blueColor = colors[0];
+let blueColor = colors[2];
 let fourthColor = colors[3];
 let fifthColor = colors[4];
 let tenthColor = colors[9]
