a611e94eea56f501071300d633623b53fbda5fe3
T <itthi91@gmail.com>
2023-12-06 11:49:33 +0700

    fix: ex7

--- ex-7.js
+++ ex-7.js
@@ -47,5 +47,5 @@ let creditCardTypeOfBlindermann = orders[2].creditCardType;
 creditCardTypeOfBlindermann = "visa";
 let totalPurchaseOfJoannet = orders[3].productQuantity;
 let totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
-delete orders[0]
+orders.shift();
 console.log(orders)
